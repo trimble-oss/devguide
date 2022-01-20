96fe4eda88133ea0ca138002a386b2a769159ed7
Christian Oliff <christian_oliff@trimble.com>
2022-01-21 01:38:00 +0900

    Add PurgeCSS to PostCSS pipeline
    remove unneeded scripts from package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
-  }
+    "@fullhuman/postcss-purgecss": {
+      content: ["./layouts/**/*.html", "./public/**/index.html", "./content/**/*.md"],
+      safelist: ["arrow", "bg-light", "chroma", "col", "container", "cookie-policy-description", "highlight", "language-txt", "ot-*", "row", "sr-only", "table", "text-light", "text-white"],
+    },
+    autoprefixer: {},
+  },
 };
