8cb44fabcd411e64b542c6ef43376d1319a83483
Ryan Kuhn <ryan_kuhn@trimble.com>
2022-01-26 11:03:15 -0700

    Removed old infrastructure and deployment code.

--- infrastructure/modules/shared/output.tf
+++
@@ -1,4 +0,0 @@
-output "functions_bucket" {
-  description = "Lambda functions bucket"
-  value       = aws_s3_bucket.functions_bucket
-}
--- infrastructure/modules/www/output.tf
+++
@@ -1,9 +0,0 @@
-output "distribution" {
-  description = "CloudFront distribution"
-  value       = aws_cloudfront_distribution.content
-}
-
-output "content_bucket" {
-  description = "Site Content Bucket"
-  value       = aws_s3_bucket.content_bucket
-}
