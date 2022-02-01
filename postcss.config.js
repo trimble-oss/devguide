5e6573cbd46ac08228cbe2456d72d9446fcc1e55
Christian Oliff <1212885+coliff@users.noreply.github.com>
2022-02-01 23:28:09 +0900

    Add flagwords to spell config
    Also fix a reference to 'master' (change to main)
    
    The Postcss config is updated so it doesn't purge the table CSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ["./layouts/**/*.html", "./public/**/index.html", "./content/**/*.md"],
-      safelist: ["arrow", "bg-light", "chroma", "col", "container", "cookie-policy-description", "highlight", "language-txt", "ot-*", "row", "sr-only", "table", "text-light", "text-white"],
+      safelist: ["arrow", "bg-light", "chroma", "col", "container", "cookie-policy-description", "highlight", "language-txt", "main", "onetrust*", "optanon*", "ot-*", "row", "sr-only", "table", "td", "text-light", "text-white", "thead"],
     },
     autoprefixer: {},
   },
