64a4e1f95e090e78b9290de7a2e50c6edd188352
Christian Oliff <christian_oliff@trimble.com>
2026-03-23 23:29:58 +0900

    Update linter configs and enable CSS linting
    Consolidate and tighten linting configuration: remove .eslintignore and .stylelintignore and move patterns into .eslintrc.json (ignorePatterns) and stylelint.config.mjs (ignoreFiles). Enable/enforce CSS linting in the super-linter workflow (add npm ci, set CSS_FILE_NAME, skip dependabot runs). Bump stylelint in package.json to 17.5.0. Add "svg" dictionary to cspell and enable additional htmlhint rules (button-type, frame-title, main-require, tag-no-obsolete).

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,10 @@
 export default {
   extends: ['stylelint-config-standard'],
+  ignoreFiles: [
+    '*.min.css',
+    '*.html',
+    '*.svg'
+  ],
   rules: {
     'color-function-notation': null,
   },
